20056425f749013730d15f7d9f39b2d967ff9686
Natanael <natanso.s17@hotmail.com>
2019-04-26 17:22:25 -0300

    Questão 3 feita e correções

--- 3Questao.rb
+++ 3Questao.rb
@@ -1,23 +1,14 @@
-class BubbleSort    
-    def self.ordenar(array)
-        tamanho_array = array.size
-        bubblesort(array,tamanho_array)
-        print(array)
-    end
-
-    def self.bubblesort(array, tamanho_array)
-        (0...tamanho_array - 1).each do |i|
-            if array[i] > array[i + 1]
-                temp = array[i]
-                array[i] = array[i + 1]
-                array[i + 1] = temp
+class QuantidadeVogais
+    @@vogais = ["a", "á", "â", "ã", "e", "é", "ê", "i", "í", "î", "o", "ó", "ô", "õ", "u", "ú", "û"]
+    def self.quantidade_vogais(string)
+        @@quantidade_vogais = 0
+        (0...string.size).each do |i|
+            if @@vogais.include?(string[i])
+                @@quantidade_vogais += 1
             end
         end
-        if tamanho_array != 1
-            bubblesort(array, tamanho_array - 1)
-        end
+        @@quantidade_vogais
     end
 end
 
-x = [5, 3, 2, 4, 7, 1, 0, 6]
-BubbleSort.ordenar(x)
+puts QuantidadeVogais.quantidade_vogais("paralelepípedo")
